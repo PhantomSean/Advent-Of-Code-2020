e1bf93fb0a5c964c1d426f075020afc0c4024813
PhantomSean <sean.stewart@ucdconnect.ie>
2020-12-06 14:11:30 +0000

    Day 6

--- Day 6/solution.py
+++ Day 6/solution.py
@@ -1,24 +1,36 @@
-from collections import Counter
+from typing import Counter
+
 
 with open("Day 6/input.txt") as file:
-    group_size = 0
+    groups = []
+    group = []
+    duplicate = []
     unique = 0
-    same_answers = 0
-    group_answers = []
+    same_ans = 0
+    group_size = 0
+    count = 0
+
     for line in file:
-        if(line != "\n"):
-            line = line.rstrip()
-            group_size += 1
-            group_answers += line
-        elif(line == "\n"):
-            duplicate = []
-            count = Counter(group_answers)
-            for letter in group_answers:
-                if (count[letter] == group_size and (letter not in duplicate)):
-                    duplicate += letter
-            unique += len(set(group_answers))
-            same_answers += len(set(duplicate))
-            group_size = 0
-            group_answers = []
+        if (line.strip()):
+            for letter in line.strip():
+                group.append(letter)
+            count += 1
+        elif (not line.strip()):
+            group.append(count)
+            groups.append(group)
+            group = []
+            count = 0
+    group.append(count)
+    groups.append(group)
+
+    for group in groups:
+        unique += len(set(group[:-1]))
+        count = Counter(group[:-1])
+        for letter in group:
+            if (count[letter] == group[-1] and letter not in duplicate):
+                duplicate.append(letter)
+        same_ans += len(set(duplicate))
+        duplicate = []
+
     print(unique)
-    print(same_answers)
+    print(same_ans)
